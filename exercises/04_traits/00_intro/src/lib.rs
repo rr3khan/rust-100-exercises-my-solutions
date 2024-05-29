c510d782de82ba81200b120816bf91bff195ae80
rr3khan <65298773+rr3khan@users.noreply.github.com>
2024-05-29 17:07:01 -0400

    Adjust 00400 and 00401 soln locations

--- exercises/04_traits/00_intro/src/lib.rs
+++ exercises/04_traits/00_intro/src/lib.rs
@@ -1,40 +1,14 @@
-// Define a trait named `IsEven` that has a method `is_even` that returns a `true` if `self` is
-// even, otherwise `false`.
-//
-// Then implement the trait for `u32` and `i32`.
-
-trait IsEven {
-    fn is_even(self) -> bool;
-}
-
-impl IsEven for u32 {
-    fn is_even(self) -> bool {
-        return self % 2 == 0;
-    }
-}
-
-impl IsEven for i32 {
-    fn is_even(self) -> bool {
-        return self % 2 == 0;
-    }
+fn intro() -> &'static str {
+    // TODO: fix me 👇
+    "I'm ready to learn about traits!"
 }
 
 #[cfg(test)]
 mod tests {
- 
-    use super::*;
+    use crate::intro;
 
     #[test]
-    fn test_u32_is_even() {
-        assert!(42.is_even());
-        assert!(!43.is_even());
-    }
-
-    #[test]
-    fn test_i32_is_even() {
-        assert!(42.is_even());
-        assert!(!43.is_even());
-        assert!(0.is_even());
-        assert!(!(-1).is_even());
+    fn test_intro() {
+        assert_eq!(intro(), "I'm ready to learn about traits!");
     }
 }
