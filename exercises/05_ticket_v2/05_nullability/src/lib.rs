79ac27aa240cb5a9bb9b8bb3942e52f565ddb143
rr3khan <65298773+rr3khan@users.noreply.github.com>
2024-06-18 00:15:25 -0400

    soln: 0505 Nullability

--- exercises/05_ticket_v2/05_nullability/src/lib.rs
+++ exercises/05_ticket_v2/05_nullability/src/lib.rs
@@ -1,4 +1,4 @@
-// TODO: Implement `Ticket::assigned_to` using `Option` as the return type.
+//  Implement `Ticket::assigned_to` using `Option` as the return type.
 
 #[derive(Debug, PartialEq)]
 struct Ticket {
@@ -36,7 +36,10 @@ impl Ticket {
         }
     }
     pub fn assigned_to(&self) -> Option<&String> {
-        todo!()
+        match &self.status {
+            Status::InProgress { assigned_to } => Some(&assigned_to),
+            _ => None,
+        }
     }
 }
 
