595c1d0b03ecab3b479393064c2df3f73e3e1d8d
rr3khan <65298773+rr3khan@users.noreply.github.com>
2024-09-02 13:05:30 -0400

    soln: 0611 Mutable slices Strings

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -2,6 +2,10 @@
 //  modifying the input in place.
 //  Does it need to take a `&mut String`? Does a `&mut [str]` work? Why or why not?
 
+fn lowercase(txt: &mut [str]) -> String {
+    return txt.to_lowercase();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
