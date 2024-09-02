4b7dee83b433b93f8b60042cd87e14f1e2bfa199
rr3khan <65298773+rr3khan@users.noreply.github.com>
2024-09-02 13:09:52 -0400

    soln: 0611 i32 Pythony soln

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,6 +1,12 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
 
+fn squared(num: &mut [i32]) {
+    for int in num {
+        *int = *int * *int
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
